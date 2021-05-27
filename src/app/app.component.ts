e621bdca2ecad3921fa0dfaf23ca1cd35f99d5f4
Lucas Santana <lsantana.2015@alunos.utfpr.edu.br>
2021-05-27 09:57:58 -0300

    conectando com a api e voltando a aparecer as imagens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,24 @@
 import { PhotoService } from './photos/photo/photo.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit{
 
   photos: any[] = [];
 
-  constructor(photoService: PhotoService){
+  constructor(private photoService: PhotoService){ }
 
-    photoService
+  ngOnInit(): void {
+
+    this.photoService
       .listFromUser('flavio')
       .subscribe(photos => this.photos = photos);
   }
+
+
  }
 
