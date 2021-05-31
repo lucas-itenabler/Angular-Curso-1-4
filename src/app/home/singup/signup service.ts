9ce76fb6e16864e15ba028ebe6bbd27a74183efb
Lucas Santana <lsantana.2015@alunos.utfpr.edu.br>
2021-05-31 10:35:45 -0300

    Validação de formulário

---
+++ src/app/home/singup/signup service.ts
@@ -0,0 +1,22 @@
+/*import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { NewUser } from './new-user';
+import { environment } from 'src/environments/environment';
+
+const API = environment.ApiUrl;
+
+@Injectable()
+export class SignUpService {
+
+  constructor(private http: HttpClient) {}
+
+  checkUserNameTaken(userName: string): Observable<Object> {
+    return this.http.get(API + '/user/exists/' + userName);
+  }
+
+  signUp(newUser: NewUser) {
+    return this.http.post(API + '/user/signup', newUser);
+  }
+}*/
