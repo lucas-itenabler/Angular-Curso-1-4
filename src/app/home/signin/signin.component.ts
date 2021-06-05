b310193ca4b2cb712f1ca2a8bc8f182eff8054bc
Lucas Santana <lsantana.2015@alunos.utfpr.edu.br>
2021-06-05 19:57:23 -0300

    Finalizando conhecimento sobre Querry Params e finalizando curso de ANGULAR pt-4

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -2,7 +2,7 @@ import { PlatformDetectorService } from './../../core/platform/platform-detector
 import { AuthService } from '../../core/auth/auth.service';
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 
 
@@ -11,6 +11,7 @@ import { Title } from '@angular/platform-browser';
 })
 export class SignInComponent implements OnInit {
 
+  fromUrl = '';
   loginForm!: FormGroup;
   @ViewChild('userNameInput') userNameInput!: ElementRef<HTMLInputElement>;
 
@@ -18,10 +19,13 @@ export class SignInComponent implements OnInit {
     private formBuilder: FormBuilder,
     private authService: AuthService,
     private router: Router,
-    private PlatformDetectorService: PlatformDetectorService ) { }
+    private PlatformDetectorService: PlatformDetectorService,
+    private activatedRoute: ActivatedRoute ) { }
 
   ngOnInit(): void {
-
+    this.activatedRoute
+    .queryParams
+    .subscribe(params => this.fromUrl = params['fromUrl']);
       this.loginForm = this.formBuilder.group({
         userName: ['', Validators.required],
         password: ['', Validators.required]
@@ -34,15 +38,22 @@ export class SignInComponent implements OnInit {
     const userName = this.loginForm.get('userName')?.value;
     const password = this.loginForm.get('password')?.value;
 
-    this.authService
-    .autenticate(userName, password)
-    .subscribe(
-      () => this.router.navigate(['user', userName]),
-      err => {
+    this.authService.autenticate(userName, password).subscribe(
+      () => this.fromUrl
+          /* if(this.fromUrl){
+                thit.router.navigateByUrl(this.fromUrl);
+            } else {                                           //ESSE "IF / ELSE" É O MEMSO QUE AS DUAS
+                this.router.navigate(['user', userName]);           PRÓXIMAS LINHAS (IF TERNÁRIO)
+            }
+          */
+          ? this.router.navigateByUrl(this.fromUrl)
+          : this.router.navigate(['user', userName]),
+      (err) => {
         console.log(err);
         this.loginForm.reset();
         this.PlatformDetectorService.isPlatformBrowser() &&
-        this.userNameInput.nativeElement.focus();
+          this.userNameInput?.nativeElement.focus();
+
         alert('Invalid user name or password');
       }
     );
